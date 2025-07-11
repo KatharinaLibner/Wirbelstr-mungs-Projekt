8a1198d72ba5acd6afa4dd3e6b1a5d948a0f8134
SnowflakeSunset <122168544+SnowflakeSunset@users.noreply.github.com>
2025-07-11 15:32:14 +0200

    Create karman.py

---
+++ karman.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import splu
+from scipy.integrate import solve_ivp
+import matplotlib.animation as animation
+from scipy.interpolate import griddata
+import time
+import matplotlib
